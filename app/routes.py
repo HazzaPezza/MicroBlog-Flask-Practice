a22cef2fb338542e6a9c13f972b6d07fbc4156b3
Harry Perriton <hjperriton@gmail.com>
2025-12-13 12:57:10 +0000

    Added a login form and some basic validation/

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 # Decorators provide URLS for following index function.
 @app.route('/')
@@ -30,3 +31,17 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Function takes decorator for login URL and HTTP methods and creates login page.
+    """
+    form = LoginForm() # Create new login form using LoginForm() function.
+
+    # If user clicks submit button.
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data)) # Return to
+        return redirect('/index')
+    return render_template('login.html', title='Sign in', form=form)
